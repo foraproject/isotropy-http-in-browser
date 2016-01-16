49055cc941237abbc71b6ed42f47b6c2a88c1367
Jeswin Kumar <jeswinpk@agilehead.com>
2016-01-16 20:21:30 +0530

    More compatible to http.ServerResponse

--- src/response.js
+++ src/response.js
@@ -3,17 +3,34 @@ import EventEmitter from 'events';
 import util from "./util";
 
 class Response extends EventEmitter {
+    statusCode: number;
+    statusMessage: string;
     finished: boolean;
     socket: { writable: boolean };
     _headers: Object;
+    headersSent: true;
+    sendDate: true;
+    body: string;
 
     constructor(params: Object) {
         super();
+        this.statusCode = -1;
+        this.statusMessage = "";
+        this.finished = false;
+        this.headersSent = true;
+        this.sendDate = true;
+        this.body = "";
+
+        this._headers = {};
+        this.socket = { writable: true };
+
         for (var key in params) {
             this[key] = params[key];
         }
-        this._headers = {};
-        this.socket = { writable: true };
+    }
+
+    addTrailers(headers: Object) {
+        this._headers = Object.assign(this._headers, headers);
     }
 
     removeHeader(name: string) {
@@ -26,9 +43,30 @@ class Response extends EventEmitter {
         this._headers[field] = val;
     }
 
+    setTimeout(msecs: number, cb: Function) : Response {
+        setTimeout(cb, msecs);
+        return this;
+    }
+
+    write(data: string) {
+        this.body += data;
+    }
+
+    writeHeader(args: any) {
+        if (args.length === 2) {
+            this.statusCode = args[0];
+            this._headers = Object.assign(this._headers, args[1]);
+        } else if (args.length === 3) {
+            this.statusCode = args[0];
+            this.statusMessage = args[1];
+            this._headers = Object.assign(this._headers, args[2]);
+        }
+    }
+
     end(body: string) {
+        this.body += body;
         this.finished = true;
-        this.emit("end", body);
+        this.emit("end", this.body);
     }
 }
 
