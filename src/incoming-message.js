97db062eacd54aa9045f62acaf984e4f44d53103
Jeswin Kumar <jeswinpk@agilehead.com>
2016-02-18 19:51:53 +0800

    Add transferEncoding and mimeType fields to annotation

--- src/incoming-message.js
+++ src/incoming-message.js
@@ -6,6 +6,8 @@ export type FormDataEntryType = {
   value?: string;
   filename?: string;
   file?: Object;
+  transferEncoding?: string;
+  mimeType?: string;
 }
 
 export type FormDataType = Array<FormDataEntryType>;
